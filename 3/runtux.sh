dbba19f04185f21c3cfb1cc2a6b5c84e7ab07bdd
Frantisek Sumsal <frantisek@sumsal.cz>
2018-04-10 14:50:17 +0200

    Suppress errors from supertux

--- 3/runtux.sh
+++ 3/runtux.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-timeout 120 supertux2 -f --disable-sound --disable-music "/usr/share/supertux2/levels/world1/01 - Welcome to Antarctica.stl"
+timeout 120 supertux2 -f --disable-sound --disable-music "/usr/share/supertux2/levels/world1/01 - Welcome to Antarctica.stl" &> /dev/null
