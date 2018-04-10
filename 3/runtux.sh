11c6d780dcab3c8fc294bc1438b6758674421213
Robin Hack <hack.robin@gmail.com>
2018-04-10 14:50:57 +0200

    runtux: The end.

--- 3/runtux.sh
+++ 3/runtux.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 timeout 120 supertux2 -f --disable-sound --disable-music "/usr/share/supertux2/levels/world1/01 - Welcome to Antarctica.stl" &> /dev/null
+
+echo
+echo
+echo "THE END! Ask for price!"
+echo
+echo
